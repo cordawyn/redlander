7bf0b538ea4035c532bd8a4053f09eff27efb313
Slava Kravchenko <cordawyn@neptune.(none)>
2012-06-13 00:01:41 +0300

    some cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,3 @@
-$:.unshift(File.join(File.dirname(__FILE__), "..")) unless
-  $:.include?(File.join(File.dirname(__FILE__), "..")) || $:.include?(File.expand_path(File.join(File.dirname(__FILE__), "..")))
-
 require 'spec/autorun'
 
 require 'lib/redlander'
